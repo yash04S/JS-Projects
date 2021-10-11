8e2f9fa8d467fb15227457ff564f9f3a96cfa79f
Yash Kumar Sharma <yashs8556@gmail.com>
2021-10-11 20:09:40 +0530

    temperature project

---
+++ Temperature/temp convertor/script1.js
@@ -0,0 +1,30 @@
+const calculateTemp = () =>{
+
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+    const celToFah =(cel) =>{
+        let fahrenheit = Math.round((cel * 9/5) + 32);
+        return fahrenheit;
+    }
+    
+    const FahToCel =(fahr) =>{
+        let celsius = Math.round((fahr - 32) * 5/9);
+        return celsius;
+    }
+
+    let result;
+
+    if(valueTemp == 'cel'){
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML= `= ${result}Fahrenheit`;
+     } else{
+        result = FahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML= `= ${result}Celsius`;
+     }
+    
+}
